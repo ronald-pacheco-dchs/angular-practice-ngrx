6ea5125a762d01d48ff2f484ac29dde6a8d62323
ronald.pacheco <ronald.pacheco@es.dachser.com>
2023-11-24 13:14:01 +0100

    :recycle: refactor: convert app to standalone component (no modules)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CommonModule, RouterOutlet],
 })
 export class AppComponent {
-  title = 'practice-ngrx';
+  
+
 }
